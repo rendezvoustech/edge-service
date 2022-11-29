0a895405e26f7610012e7232a56a4765d2c337c9
Mikkel Arentoft <mikkel@mikadocs.com>
2022-11-29 16:29:51 +0100

    First implementation

---
+++ src/main/java/tech/rendezvous/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.rendezvous.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServiceApplication.class, args);
+    }
+
+}
